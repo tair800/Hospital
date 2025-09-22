4d2ec08f02ec1b3526ff358ccbe25dfb5a521365
tair800 <146845940+tair800@users.noreply.github.com>
2025-09-22 15:15:05 +0400

    Work in progress before moving to main

--- HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
+++ HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5ee4c064f018a37b2f0dfcd1f6333841f984b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9dcb7216ac3681ccbf10a4f869d69e2f6e860c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
